e905534f4bf788401c1866b444e9b7bc3cd66a1a
Ördög Csaba <ordog.csaba.0@gmail.com>
2022-04-29 21:39:38 +0200

    több scene

--- src/main/java/kepnezegeto/megjelenites/KezeloFelulet.java
+++ src/main/java/kepnezegeto/megjelenites/KezeloFelulet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class KezeloFelulet extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(KezeloFelulet.class.getResource("asd.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(KezeloFelulet.class.getResource("nyitoScene.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Kepnezegeto");
--- src/main/java/kepnezegeto/megjelenites/KezeloFeluletController.java
+++ src/main/java/kepnezegeto/megjelenites/KezeloFeluletController.java
@@ -2,52 +2,40 @@ package kepnezegeto.megjelenites;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import kepnezegeto.filterek.Grayscale;
-import kepnezegeto.kepek.Jpg;
-import kepnezegeto.kepek.Kep;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class KezeloFeluletController {
-    @FXML
-    public Slider forgatasMertek;
-    public Label forgatasLabel;
 
     @FXML
     protected TextField eleresiUt;
 
-    @FXML
-    protected ImageView kepMegjelenit;
+
 
     @FXML
     protected void valami() {
         System.out.println("hello");
     }
-    public void initialize() {
 
-        forgatasMertek.valueProperty().addListener((observable, oldValue, newValue) -> {
-            int csuszkaErtek = (int)Math.round((Double)newValue);
-            kepMegjelenit.setRotate(csuszkaErtek);
-            forgatasLabel.setText(String.valueOf(csuszkaErtek));
-
-        });
-
-    }
     @FXML
-    protected void fajlMegnyit(){
+    protected void fajlMegnyit(MouseEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene ujAblak = new Scene( new FXMLLoader(KezeloFelulet.class.getResource("kepszerkesztoScene.fxml")).load(), 800, 600);
+        stage.setScene(ujAblak);
+        /*
         if(!eleresiUt.getText().isBlank()) {
             Image megnyitottKep = (new Jpg(eleresiUt.getText()).createImage());
             megnyitottKep = Grayscale.alkalmazFilter(megnyitottKep);
             kepMegjelenit.setImage(megnyitottKep);
 
         }
-
+*/
     }
 
 
